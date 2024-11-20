3c684f5d070fac8138ab128fe572fa6c86bfc624
John Arocha <john.arocha@clover.com>
2024-11-20 11:57:17 -0700

    added logic for addint to JSON db

--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,4 +1,10 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
 public class Console {
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
 
     public void start(Boolean menu) {
@@ -19,6 +25,15 @@ public class Console {
         switch (userChoice) {
             case 1:
                 System.out.println("Create Employee");
+                Employee employee = new Employee("John", "Doe", 24, "test@test.com", "test");
+                try {
+                    objectMapper.writeValue(new File("src/main/db/db.json"), employee);
+                    System.out.println("Employee successfully created");
+                } catch (IOException e) {
+                    System.out.println("Error occurred: ");
+                    System.out.println(e);
+                }
+
                 break;
             case 2:
                 System.out.println("Delete Employee");
